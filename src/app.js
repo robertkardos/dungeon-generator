b729c43fc8c70c94e28676fb794b4c78b041e8bd
Robert Kardos <kardos.robert.kh@gmail.com>
2017-02-26 01:45:15 +0100

    Put labyrinth config in separate file

--- src/app.js
+++ src/app.js
@@ -4,29 +4,6 @@ import * as _ from 'underscore';
 import { Labyrinth } from './labyrinth';
 import { util } from './util';
 
-let labyrinthConfig = {
-	width          : 20,
-	height         : 20,
-	// wallThickness  : 1, // default : 10
-	size           : 5,
-	// step           : 10,
-	randomness     : 4,
-	write          : false,
-	// ereaseDeadEnds : 0,
-	rooms          : {
-		roomAttempts   : 30,
-		pRoomWidthMax  : 5,
-		pRoomWidthMin  : 1,
-		pRoomHeightMax : 5,
-		pRoomHeightMin : 1
-	}
-};
+import { config } from './config';
 
-let labyrinth = new Labyrinth(labyrinthConfig);
-
-// for (var x = 0; x < labyrinth.width; x++) {
-// 	for (var y = 0; y < labyrinth.height; y++) {
-// 		canvas.fillStyle = util.tileTypes[labyrinth.map[x][y]].color;
-// 		canvas.fillRect(x * 5, y * 5, 5, 5);
-// 	}
-// }
+let labyrinth = new Labyrinth(config);
---
+++ src/config.js
@@ -0,0 +1,19 @@
+let config = {
+	width          : 70,
+	height         : 70,
+	// wallThickness  : 1, // default : 10
+	size           : 16,
+	// step           : 10,
+	randomness     : 4,
+	write          : true,
+	// ereaseDeadEnds : 0,
+	rooms          : {
+		roomAttempts   : 30000,
+		pRoomWidthMax  : 7,
+		pRoomWidthMin  : 1,
+		pRoomHeightMax : 7,
+		pRoomHeightMin : 1
+	}
+};
+
+export { config };
